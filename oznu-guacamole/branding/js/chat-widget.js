912deb4f5d151e38d5c13d96ea451083677dfe66
EC2 Default User <ec2-user@ip-172-31-47-81.ec2.internal>
2021-08-02 19:04:47 +0000

    changes

---
+++ oznu-guacamole/branding/js/chat-widget.js
@@ -0,0 +1,2 @@
+<script type="text/javascript">var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "9309d40e30e38e62cfc650afd202a3117a35959e9a9c4b5f9ef804dfb69f6c781a2010ab7b6727677d37b27582c0e9c4", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
+</script>
